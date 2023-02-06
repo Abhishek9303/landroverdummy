019322970dbce2c39c89fd4df070724455569166
Abhishek <abhishekvishwakarma3244@gmail.com>
2023-02-06 11:20:29 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// import LocomotiveScroll from 'locomotive-scroll';
+
+// const scroll = new LocomotiveScroll();
+// const target = document.querySelector('#main');
+
+// scroll.scrollTo(target);
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
